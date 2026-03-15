5b0952d833fe8259de12721d1b095c243062d68e
jaehoon <jaehun4086@naver.com>
2026-03-15 17:34:08 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        int count = 0;
+        
+        if(num == 1)
+            return 0;
+        
+        while(num != 1){
+            
+            if(num % 2 == 0){
+                num /= 2;
+            } else{
+                num = (num * 3) + 1;
+            }
+            answer++;
+            
+            if(answer == 500){
+                answer = -1;
+                break;
+            }
+        }
+        return answer;
+        
+    }
+}
