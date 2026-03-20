701ea6e7beeceae5701e5606c6025c982737bf8e
jaehoon <jaehun4086@naver.com>
2026-03-20 14:13:22 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 97.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        
+        long[] dp = new long[n + 1];
+        
+        dp[1] = 1;
+        if(n > 1){
+            dp[2] = 2;
+        }
+        
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+        
+        return dp[n] % 1234567;
+    }
+}
