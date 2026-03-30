306202dc4ea8a631f6cc7c677de916f9b283a8a6
jaehoon <jaehun4086@naver.com>
2026-03-30 14:10:46 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.65 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        // 세로 길이 2, 가로 n
+        // n = 1 => 1가지
+        // n = 2 => 2가지
+        // n = 3 => 3가지
+        // n = 4 => 5가지
+        
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+        return dp[n];
+    }
+}
