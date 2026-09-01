6e74b3b0734aedfec441afe1b06a348909a8825d
jaehoon <jaehun4086@naver.com>
2026-09-01 13:33:23 +0900

    [level 2] Title: 숫자의 표현, Time: 0.67 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,25 +1,37 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        int answer = 1;
         
-        int start = 1;
+        if(n == 1)
+            return 1;
+        
+        
+        int[] arr = new int[n];
+        
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = i + 1;
+        }
+        
+        int start = 0;
         int end = 1;
-        int sum = 1;
         
-        while(end <= n){
+        int sum = arr[start] + arr[end];
+        while(end  < n - 1){
             if(sum == n){
+                // 타겟일 때
                 answer++;
-                sum -= start;
-                start++;
-            } else if(sum < n){
                 end++;
-                sum += end;
+                sum += arr[end];
+           } else if(sum < n){
+                // 작을 때
+                end++;
+                sum += arr[end];
             } else if(sum > n){
-                sum -= start;
+                // 클 때
+                sum -= arr[start];
                 start++;
             }
         }
-        
         return answer;
     }
 }
