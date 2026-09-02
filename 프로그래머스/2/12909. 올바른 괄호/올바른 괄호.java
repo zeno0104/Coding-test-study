a16a4b62acf5603157d21f9a3798fb39491657b1
jaehoon <jaehun4086@naver.com>
2026-09-02 13:50:41 +0900

    [level 2] Title: 올바른 괄호, Time: 11.30 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,22 +3,23 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
+
+        Stack<Character> stack = new Stack();
         
-        Stack<Character> stack = new Stack<>();
-            
-        for(char ch : s.toCharArray()){
-            if(ch == '('){
-                stack.push(ch);
-            } else if(ch == ')'){
-                if(stack.isEmpty()){
-                    return false;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push('(');
+            } else if(!stack.isEmpty() && s.charAt(i) == ')'){
+                if(stack.peek() == '('){
+                    stack.pop();
                 }
-                stack.pop();
+            } else{
+                answer = false;
+                break;
             }
         }
-        if(stack.isEmpty())
-            return true;
-        else
-            return false;
+        
+        return stack.isEmpty() && answer ? true : false;
     }
 }
