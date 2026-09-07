4a23424cedcfa459da28f4357462abb5be6da2d2
jaehoon <jaehun4086@naver.com>
2026-09-07 15:31:54 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 10.84 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -2,21 +2,36 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         
-        String[] str = s.split(" ", -1);
-      
-        for(int i = 0; i < str.length; i++){
-            for(int j = 0; j < str[i].length(); j++){
-                if(j % 2 == 0){
-                    answer += Character.toUpperCase(str[i].charAt(j));
-                } else{
-                    answer += Character.toLowerCase(str[i].charAt(j));
-                }
-            }
-            if(i == str.length - 1)
-                break;
-            answer += " ";
-        }
+        String[] str = s.split(" ");
         
+        int count = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == ' '){
+                answer += " ";
+                count = 0;
+            } else if(count % 2 == 0){
+                answer += Character.toUpperCase(s.charAt(i));
+                count++;
+            } else{
+                answer += Character.toLowerCase(s.charAt(i));
+                count++;
+            }
+           
+            
+            // String str2 = "";
+            // for(int j = 0; j < str[i].length(); j++){
+            //     if(j % 2 == 0){
+            //         str2 += Character.toUpperCase(str[i].charAt(j));
+            //     } else{
+            //         str2 += Character.toLowerCase(str[i].charAt(j));
+            //     }
+            // }
+            // answer += str2;
+            // if(i != str.length - 1){
+            //     answer += " ";
+            // }
+        }
         return answer;
     }
 }
