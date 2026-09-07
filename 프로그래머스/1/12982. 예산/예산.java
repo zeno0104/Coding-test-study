2bd56b2c894ace9f4b8bfe88b4a6e7721411cf17
jaehoon <jaehun4086@naver.com>
2026-09-07 15:18:36 +0900

    [level 1] Title: 예산, Time: 3.87 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,21 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        
+        int sum = 0;
         Arrays.sort(d);
         
-        int sum = 0;
-        
-        // 1 2 3 4 5
-        
         for(int i = 0; i < d.length; i++){
-            if(sum + d[i] > budget){
-                break;
-            }
             sum += d[i];
-            answer++;
+            if(sum == budget){
+                answer = i + 1;
+                break;
+            } else if(sum >= budget){
+                answer = i;
+                break;
+            } else if(i == d.length - 1){
+                answer = i + 1;
+            }
         }
         
         return answer;
     }
+    
 }
