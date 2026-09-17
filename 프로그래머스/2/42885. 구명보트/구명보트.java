5964ee1d792aeebe7cd275a1e579dbaec98694e5
jaehoon <jaehun4086@naver.com>
2026-09-17 15:49:05 +0900

    [level 2] Title: 구명보트, Time: 9.11 ms, Memory: 58.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,21 +3,26 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        Arrays.sort(people);
         
+        Arrays.sort(people);
+
         int left = 0;
         int right = people.length - 1;
         
         while(left <= right){
-            if(people[left] + people[right] <= limit){
+            int sum = people[left] + people[right];
+            
+            if(sum > limit){
                 answer++;
+                right--;
+            }  else {
                 left++;
                 right--;
-            } else{
                 answer++;
-                right--;
             }
         }
+        
+        
         return answer;
     }
 }
