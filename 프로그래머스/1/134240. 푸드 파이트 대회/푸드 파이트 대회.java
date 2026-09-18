c0f4737143a0b270775feedca495623ef7ae6b14
jaehoon <jaehun4086@naver.com>
2026-09-18 11:08:26 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 14.11 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,21 +2,37 @@ class Solution {
     public String solution(int[] food) {
         String answer = "";
         
+        int start = 1;
+        
         for(int i = 1; i < food.length; i++){
-            if(food[i] / 2 >= 1){
-                for(int j = 0 ; j < food[i] / 2; j++){
-                    answer += i;
-                }
+            
+            if(food[i] == 1){
+                start++;
+                continue;
             }
+            
+            int eat = food[i] / 2;
+            System.out.println("eat : " + eat);
+            
+            for(int j = 0; j < eat; j++){
+                answer += String.valueOf(start);
+            }
+
+            start++;
         }
-        answer += 0;
         
+        answer += "0";
         
-        for(int i = answer.length() - 2;  i >= 0; i--){
-            answer += answer.charAt(i);
+        String newAnswer = "";
+        
+        for(int i = answer.length() - 2; i >= 0; i--){
+            newAnswer += answer.charAt(i);
         }
+        answer += newAnswer;
         
-        System.out.println(answer);
+        System.out.println("answer : " + answer);
+        
+        
         return answer;
     }
 }
