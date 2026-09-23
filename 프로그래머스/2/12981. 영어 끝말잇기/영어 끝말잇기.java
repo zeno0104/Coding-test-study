e50cb17e42f05387397865f7b267d533183150b9
jaehoon <jaehun4086@naver.com>
2026-09-23 10:52:04 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -3,39 +3,30 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = {};
-        // 몇번째 사람이
-        // 몇번째에 떨어지냐
         
-        Stack<String> stack = new Stack();
+        Set<String> set = new HashSet();
+        
         boolean flag = false;
+        int nCount = 0;
+        
+        String before = words[0];
+        set.add(words[0]);
         
         int first = 0;
         int second = 0;
-        
-        for(int i = 0; i < words.length; i++){
-            if(stack.isEmpty()){
-                stack.add(words[i]);
+        for(int i = 1; i < words.length; i++){
+            if(set.contains(words[i]) || before.charAt(before.length() - 1) != words[i].charAt(0)) {
+                // 포함되었을 때
+                flag = true;
+                first = (i % n) + 1;
+                second = (i / n) + 1;
+                break;
             } else{
-                String curr = stack.peek();
-                char lastWord = curr.charAt(curr.length() - 1); 
-                
-                if(stack.contains(words[i]) || lastWord != words[i].charAt(0)){
-                    // 마지막 글자와 다를 때
-                    flag = true;
-                    first = (i % n) + 1;
-                    second = (i / n) + 1;
-                    break;
-                } 
-                else{
-                    stack.add(words[i]);
-                } 
+                before = words[i];
+                set.add(words[i]);
             }
         }
-       
         
-        if(flag){
-            return new int[]{first, second};
-        } 
-        return new int[]{0, 0};
+        return !flag ? new int[]{0, 0} : new int[]{first, second};
     }
 }
